03f68a334750a2c18d5b6ea8e177fc88bc17c896
AnikaTahsin06 <bushratahsin653@gmail.com>
2024-04-29 23:45:39 +0600

    started to make controls buttons functional

---
+++ app.js
@@ -0,0 +1,7 @@
+const container = document.querySelectorAll('.container');
+const allBtn = document.querySelectorAll('.controls');
+const singleBtn = document.querySelectorAll('.control');
+const allContainers = document.querySelector('.main-content');
+
+console.log('array: ', [container, allBtn, singleBtn, allContainers])
+
